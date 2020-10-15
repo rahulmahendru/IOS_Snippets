f4061e11f16600d9e1076bd586e5da8719433965
rahulmahendru <rkm687304@gmail.com>
2020-10-15 17:44:12 +0530

    Task Bar Snippet Initialized

---
+++ TaskBar/TaskBar/TaskBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskBarApp.swift
+//  TaskBar
+//
+//  Created by Rahul Mahendru on 10/15/20.
+//
+
+import SwiftUI
+
+@main
+struct TaskBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
